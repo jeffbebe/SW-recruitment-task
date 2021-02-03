96f2a06b5cadc7203ccdb96b0bcf0c22ccd94ef5
jeffbebe <oskarw46@gmail.com>
2021-02-03 21:15:31 +0100

    Complete swagger doc, minor cleanup

--- src/routes/auth.router.ts
+++ src/routes/auth.router.ts
@@ -3,25 +3,56 @@ import { postSignup, postLogin } from "../controllers/auth.controller";
 
 export const authRouter = express.Router();
 
-authRouter.post("/auth/signup", postSignup);
 authRouter.post(
-  "/auth/login",
-  postLogin
-  /* 	#swagger.tags = ['Login']
-      #swagger.description = 'Endpoint to log in' 
-      #security: bearerAuth[]*/
+  "/auth/signup",
+  postSignup
+  /* 	#swagger.tags = ['Auth']
+      #swagger.description = 'Endpoint to signup ' */
 
   /*	#swagger.parameters['data'] = {
           in: 'body',
-          description: 'User information.',
+          description: 'User information',
           required: true,
           type: 'object',
         schema: {
-            $email: "mail@gmail.com", 
+            $email: "mail@mail.com", 
+            $password: "password",
+            $confirmPassword: "password"
+        }}
+  } */
+  /* #swagger.responses[200] = { 
+            description: 'User successfully added'} 
+    } */
+  /* #swagger.responses[400] = { 
+            description: 'Wrong credentials, user already exists or passwords do not match'} 
+    } */
+  /* #swagger.responses[500] = { 
+            description: 'Internal error'} 
+    } */
+);
+authRouter.post(
+  "/auth/login",
+  postLogin
+  /* 	#swagger.tags = ['Auth']
+      #swagger.description = 'Endpoint to log in' */
+
+  /*	#swagger.parameters['data'] = {
+          in: 'body',
+          description: 'User information',
+          required: true,
+          type: 'object',
+        schema: {
+            $email: "mail@mail.com", 
             $password: "password"
         }}
   } */
   /* #swagger.responses[200] = { 
-                    description: 'User successfully obtained.'} 
-            } */
+            description: 'User successfully obtained.'} 
+    } */
+  /* #swagger.responses[400] = { 
+            description: 'Wrong credentials, user already exists or passwords do not match'} 
+    } */
+  /* #swagger.responses[500] = { 
+            description: 'Internal error'} 
+    } */
 );
--- src/routes/resources.router.ts
+++ src/routes/resources.router.ts
@@ -10,7 +10,7 @@ resourcesRouter.get(
   "/:resources/:id",
   isAuth,
   getSingleResource
-  /* 	#swagger.tags = ['Get resources']
+  /* 	#swagger.tags = ['Resources']
       #swagger.description = 'Endpoint to get certain resource assigned to a user' 
       #swagger.security = [{
             "bearerAuth": []
@@ -18,18 +18,35 @@ resourcesRouter.get(
   */
 
   /* #swagger.responses[200] = { 
-    description: 'Data fetched successfully'} 
+        description: 'Data fetched successfully'} 
+  } */
+  /* #swagger.responses[400] = { 
+        description: 'Invalid token data'} 
     } */
+  /* #swagger.responses[401] = { 
+        description: 'Unauthorized, wrong token'} 
+  } */
+  /* #swagger.responses[403] = { 
+        description: 'Forbidden, this resource does not belong to a user'} 
+  } */
 );
 resourcesRouter.get(
   "/:resources",
   isAuth,
   getResources
-  /* 	#swagger.tags = ['Get resources']
-      #swagger.description = 'Endpoint to get certain resources assigned to a user' 
-      #security: bearerAuth[]*/
+  /* 	#swagger.tags = ['Resources']
+      #swagger.description = 'Endpoint to get all resources of a given name assigned to a user' 
+      #swagger.security = [{
+            "bearerAuth": []
+      }]*/
 
   /* #swagger.responses[200] = { 
-                    description: 'Data fetched successfully'} 
-            } */
+        description: 'Data fetched successfully'} 
+  } */
+  /* #swagger.responses[400] = { 
+        description: 'Invalid token data'} 
+    } *
+  /* #swagger.responses[401] = { 
+        description: 'Unauthorized, wrong token'} 
+  } */
 );
